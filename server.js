59f54e2e6e908fb3d5e54f4fef73db7cc9cef88e
Cosmizy <116780823+Cosmizy@users.noreply.github.com>
2026-05-15 09:42:43 -0700

    Update server.js

--- server.js
+++ server.js
@@ -519,16 +519,9 @@ function pointInTri(px, py, t){
   return u >= 0 && v >= 0 && (u + v < 1);
 }
 
-function spikesKill(p) {
-  for (const s of spikes) {
-    const h = s.hitbox;
-
-    if (
-      p.x > h.x &&
-      p.x < h.x + h.w &&
-      p.y > h.y &&
-      p.y < h.y + h.h
-    ) {
+function spikesKill(p){
+  for (const t of spikes) {
+    if (pointInTri(p.x, p.y, t)) {
       return true;
     }
   }

cb1a91c33db08f94b871610d76f61cfc0b2ad2c3
Cosmizy <116780823+Cosmizy@users.noreply.github.com>
2026-05-15 10:02:37 -0700

    Update server.js

--- server.js
+++ server.js
@@ -190,6 +190,8 @@ io.on("connection", socket => {
   const room = rooms[roomName];
   if (!room) return;
 
+  if (socket.room === roomName) return;
+
   const count = Object.keys(room.players).length;
 
   if (count >= MAX_PLAYERS) {
